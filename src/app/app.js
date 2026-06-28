1f7f97abecd089a8cd521c1f56e97f720ac2a9fe
codeinfinite19 <shahedhassan572@gmail.com>
2026-06-28 21:39:33 +0600

    server is initialize

---
+++ src/app/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Fable API Running 🚀",
+  });
+});
+
+export default app;
