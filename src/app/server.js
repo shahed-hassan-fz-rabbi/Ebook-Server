618cc499c9899b2c86db6cfaeb920cb69c1a37e6
codeinfinite19 <shahedhassan572@gmail.com>
2026-06-28 23:41:26 +0600

    server is updated

--- src/app/server.js
+++ src/app/server.js
@@ -1,7 +1,21 @@
+import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./app/config/db.js";
+
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+startServer();
