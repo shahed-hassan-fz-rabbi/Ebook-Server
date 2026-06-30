3609f867ddf08b9678a5581067cd5ba8e5990900
codeinfinite19 <shahedhassan572@gmail.com>
2026-06-30 15:05:18 +0600

    imgbb is activated

--- src/seed.js
+++ src/seed.js
@@ -26,56 +26,47 @@ const seed = async () => {
         title: "The Silent Storm",
         description: "A thrilling mystery novel.",
         content: "Full ebook content...",
-        coverImage:
-          "https://placehold.co/400x600/png",
+        coverImage: "https://i.ibb.co/hFZmG5Pz/book7.jpg",
         genre: "Thriller",
         language: "English",
         price: 10,
         author: writer._id,
       },
-
       {
         title: "Echoes of Tomorrow",
         description: "Sci-Fi Adventure",
         content: "Full ebook content...",
-        coverImage:
-          "https://placehold.co/400x600/png",
+        coverImage: "https://i.ibb.co/Z11k7YmH/book3.jpg",
         genre: "Sci-Fi",
         language: "English",
         price: 12,
         author: writer._id,
       },
-
       {
         title: "Crimson Petals",
         description: "Romantic Story",
         content: "Full ebook content...",
-        coverImage:
-          "https://placehold.co/400x600/png",
+        coverImage: "https://i.ibb.co/4B8dXyr/book6.jpg",
         genre: "Romance",
         language: "English",
         price: 9,
         author: writer._id,
       },
-
       {
         title: "Dragon Kingdom",
         description: "Fantasy Story",
         content: "Full ebook content...",
-        coverImage:
-          "https://placehold.co/400x600/png",
+        coverImage: "https://i.ibb.co/jZyMJsLL/book5.jpg",
         genre: "Fantasy",
         language: "English",
         price: 14,
         author: writer._id,
       },
-
       {
         title: "Haunted House",
         description: "Horror Story",
         content: "Full ebook content...",
-        coverImage:
-          "https://placehold.co/400x600/png",
+        coverImage: "https://i.ibb.co/67CFJXj7/book4.jpg",
         genre: "Horror",
         language: "English",
         price: 8,
